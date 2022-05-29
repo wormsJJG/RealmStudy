f91beb4080119d81dc35d37f4ed83338f0c89842
wormsJJG <jjr66461837@gmail.com>
2022-05-29 20:08:19 +0900

    Initial Commit

---
+++ RealmStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmStudy
+//
+//  Created by 정재근 on 2022/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RealmStudyUITests/RealmStudyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RealmStudyUITestsLaunchTests.swift
+//  RealmStudyUITests
+//
+//  Created by 정재근 on 2022/05/29.
+//
+
+import XCTest
+
+class RealmStudyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
